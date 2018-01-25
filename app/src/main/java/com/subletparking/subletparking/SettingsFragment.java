65b70a883d9e6c6091d5cd087f0850c8c4ec9d52
amitaima <amitai.malka@gmail.com>
2018-01-25 18:05:43 +0200

    changed UI - button for opening navigation drawer and more

--- app/src/main/java/com/subletparking/subletparking/SettingsFragment.java
+++ app/src/main/java/com/subletparking/subletparking/SettingsFragment.java
@@ -3,9 +3,12 @@ package com.subletparking.subletparking;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by User on 12/18/2017.
@@ -14,11 +17,25 @@ import android.view.ViewGroup;
 public class SettingsFragment extends Fragment {
 
     View myView;
+    ImageButton menuButton;
+    DrawerLayout mDrawerLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.settings_layout, container, false);
+
+        menuButton = (ImageButton) myView.findViewById(R.id.menuButton1);
+        mDrawerLayout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+
+            }
+        });
+
         return myView;
     }
 }
